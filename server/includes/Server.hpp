e05ed9d274b4c5aa4e9970e50f4949e9af19b824
EwenBriand <ewen.briand@epitech.eu>
2024-11-17 07:09:47 +0100

    [ADD] Start of server, can send and recieve from client, proto routes

---
+++ server/includes/Server.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include "Chats.hpp"
+
+#include <boost/asio.hpp>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <thread>
+
+class Clients;
+
+using boost::asio::ip::tcp;
+
+class Server {
+public:
+    Server(int port);
+    ~Server();
+    void handleNewClient(tcp::socket socket);
+    void broadcast(std::string msg);
+
+private:
+    boost::asio::io_context io_context;
+    tcp::acceptor acceptor;
+    std::mutex map_mutex;
+
+    std::map<tcp::endpoint, std::shared_ptr<Client>> _listClient;
+    std::map<std::string, Chats> _listChats;
+};
+
+/// thread qui listen en boucle et feed un buufer circulaire
+/// main thread qui feed le bon client en fonction de la provenance
+/// client executes ses commandes
---
+++ server/src/main.cpp
@@ -0,0 +1,8 @@
+#include "Server.hpp"
+
+int main()
+{
+    auto serv = new Server(8080);
+
+    return 0;
+}
