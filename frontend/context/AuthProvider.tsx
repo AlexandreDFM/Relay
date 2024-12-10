d68cef7ec42d3ebdc2620e7a4cacf4e390b3b122
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2024-12-10 19:02:31 +0900

    feature(handle): handle to log to server in already logged previously

--- frontend/context/AuthProvider.tsx
+++ frontend/context/AuthProvider.tsx
@@ -1,6 +1,8 @@
 import { useSegments, router } from "expo-router";
-import { createContext, useContext, useEffect, useState } from "react";
+import { useWebSocket } from "./WebsocketProvider";
+import useServerManager from "@/hook/useServerManager";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, useContext, useEffect, useState } from "react";
 
 type User = {
     email: string;
@@ -10,6 +12,7 @@ type User = {
 
 type AuthType = {
     user: User | null;
+    isLogged: boolean;
     setUser: (user: User | null) => void;
     clearUser: () => void;
 };
@@ -20,6 +23,7 @@ const AuthContext = createContext<AuthType>({
         password: "",
         accessToken: "",
     },
+    isLogged: false,
     setUser: () => {},
     clearUser: () => {},
 });
@@ -44,7 +48,10 @@ export function AuthProvider({
 }: {
     children: JSX.Element;
 }): JSX.Element {
+    const { isConnected } = useWebSocket();
+    const { connectClient } = useServerManager();
     const [user, setUser] = useState<User | null>(null);
+    const [isLogged, setIsLogged] = useState<boolean>(false);
 
     useEffect(() => {
         const loadUser = async () => {
@@ -53,6 +60,7 @@ export function AuthProvider({
                 setUser(JSON.parse(userString));
             }
         };
+
         loadUser();
     }, []);
 
@@ -66,12 +74,36 @@ export function AuthProvider({
                 await AsyncStorage.removeItem("user");
             }
         };
+
         storeUser();
     }, [user]);
+
+    useEffect(() => {
+        const handleLoginWithCredentials = async () => {
+            if (isLogged) return;
+            if (!(isConnected && user)) return;
+            const response = await connectClient(0, user.email, user.password);
+            if (response === "200 You are connected\n") {
+                console.log("Client connected and approved!");
+                setUser({
+                    ...user,
+                    accessToken: response,
+                });
+                setIsLogged(true);
+            } else {
+                console.error("Connection failed:", response);
+                setUser(null);
+                setIsLogged(false);
+            }
+        };
+
+        handleLoginWithCredentials();
+    }, [isConnected, user]);
 
     const authContext: AuthType = {
         user,
         setUser,
+        isLogged,
         clearUser: () => setUser(null),
     };
 
