d37950bda3e018eac5a5b4b00fb83a7915ff531a
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2024-12-12 17:03:14 +0900

    feature(frontend:broadcast): + broadcast on front & remove cursed throw

--- frontend/context/WebsocketProvider.tsx
+++ frontend/context/WebsocketProvider.tsx
@@ -9,14 +9,18 @@ import React, {
 type WebSocketContextType = {
     ws: WebSocket | null;
     isConnected: boolean;
+    broadcast: string;
     sendMessage: (message: string) => Promise<string>;
 };
 
 const WebSocketContext = createContext<WebSocketContextType>({
     ws: null,
     isConnected: false,
+    broadcast: "",
     sendMessage: async () => {
-        throw new Error("WebSocket is not connected.");
+        // throw new Error("WebSocket is not connected.");
+        console.error("WebSocket is not connected.");
+        return "";
     },
 });
 
@@ -28,6 +32,7 @@ export const WebSocketProvider = ({ children }: { children: JSX.Element }) => {
     const [ws, setWs] = useState<WebSocket | null>(null);
     const [isConnected, setIsConnected] = useState(false);
     const [listeners, setListeners] = useState(new Map());
+    const [broadcast, setBroadcast] = useState<string>("");
 
     useEffect(() => {
         const socket = new WebSocket("ws://127.0.0.1:8080");
@@ -35,6 +40,11 @@ export const WebSocketProvider = ({ children }: { children: JSX.Element }) => {
 
         const handleMessage = (event: MessageEvent) => {
             const message = event.data;
+            if (message.startsWith("600")) {
+                const broadcastMessage = message.slice(10);
+                setBroadcast(broadcastMessage);
+                return;
+            }
             console.log("Received message:", message);
             listeners.forEach((listener) => listener(message));
         };
@@ -65,7 +75,8 @@ export const WebSocketProvider = ({ children }: { children: JSX.Element }) => {
     const sendMessage = useCallback(
         (message: string): Promise<string> => {
             if (!ws || ws.readyState !== WebSocket.OPEN) {
-                throw new Error("WebSocket is not connected.");
+                // throw new Error("WebSocket is not connected.");
+                console.error("WebSocket is not connected.");
             }
 
             return new Promise((resolve) => {
@@ -76,7 +87,7 @@ export const WebSocketProvider = ({ children }: { children: JSX.Element }) => {
                 const listenerId = Symbol("listener");
                 listeners.set(listenerId, handleResponse);
 
-                ws.send(message);
+                if (ws) ws.send(message);
 
                 const cleanup = () => listeners.delete(listenerId);
                 return cleanup;
@@ -86,7 +97,9 @@ export const WebSocketProvider = ({ children }: { children: JSX.Element }) => {
     );
 
     return (
-        <WebSocketContext.Provider value={{ ws, isConnected, sendMessage }}>
+        <WebSocketContext.Provider
+            value={{ ws, isConnected, sendMessage, broadcast }}
+        >
             {children}
         </WebSocketContext.Provider>
     );
